a4c7fb70e42ea5b79644ac77e958baefc8cfe9c5
Naveen Hiranya <naveenhiranya999@gmail.com>
2026-08-02 22:31:03 +0530

    data update

--- public/data/movies.ts
+++ public/data/movies.ts
@@ -1,21 +1,13 @@
-export interface Movie {
-  id: string;
-  title: string;
-  slug: string;
-  image: string;
-  year: string;
-}
-
 export const MOVIES: Movie[] = [
   {
-    id: "1315772", // This is the ID from your example
+    id: "1315772",
     title: "Minions and Monsters",
     slug: "minions-and-monsters",
     image: "https://static1.showtimes.com/poster/160x236/minions-monsters-289770.jpg",
     year: "2026",
   },
   {
-    id: "969681", 
+    id: "969681",
     title: "Spider-Man: Brand New Day",
     slug: "spiderman-brand-new-day",
     image: "https://image.tmdb.org/t/p/w500/iPOn6DinuVyLY17YM9mKuPofV08.jpg",
@@ -23,9 +15,44 @@ export const MOVIES: Movie[] = [
   },
   {
     id: "1368337",
-    title: "Odyssey",
-    slug: "odyssey",
+    title: "The Odyssey",
+    slug: "the-odyssey",
     image: "https://tse1.mm.bing.net/th/id/OIP.pX4sLTt16K2YnEn-UTD7ewAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    year: "2026",
+  },
+  {
+    id: "1081003",
+    title: "Supergirl",
+    slug: "supergirl",
+    image: "https://th.bing.com/th?id=OIF.P%2fgfbFpH3TeCs%2fOgIZm2ZQ&r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    year: "2026",
+  },
+  {
+    id: "1339713",
+    title: "Obsession",
+    slug: "obsession",
+    image: "https://tse1.mm.bing.net/th/id/OIF.sR6wIm22zwKDpMVtGZ8vGA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    year: "2026",
+  },
+  {
+    id: "454639",
+    title: "Masters of the Universe",
+    slug: "masters-of-the-universe",
+    image: "https://th.bing.com/th?id=OIF.%2fmHdI5h%2bBg9hXvh7kiF1Cw&r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    year: "2026",
+  },
+  {
+    id: "1108427",
+    title: "Moana",
+    slug: "moana",
+    image: "https://tse3.mm.bing.net/th/id/OIP.98mGSxXJVT1Z96Gwtcq97gHaK-?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
+    year: "2026",
+  },
+  {
+    id: "1084244",
+    title: "Toy Story 5",
+    slug: "toy-story-5",
+    image: "https://tse4.mm.bing.net/th/id/OIP.7jllGCm-SalkABWTnR4UJgHaLH?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
     year: "2026",
   },
 ];
